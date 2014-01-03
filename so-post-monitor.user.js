72e1fe04b2083574eecdf6d97e1b69d2f9834f6f
Jan Dvorak <honnza@gmail.com>
2014-01-03 14:20:59 +0100

    uploaded v1.0 of so-post-monitor

---
+++ so-post-monitor.user.js
@@ -0,0 +1,115 @@
+// ==UserScript==
+// @name          Stack Exchange post monitor
+// @description   a hacky bugfix of Stack Exchange questions not updating properly. 
+// @description   currently fixes the posts not updating their deleted/undeleted status.
+// @include       http://*.stackexchange.com/*
+// @include       http://*.stackoverflow.com/*
+// @include       http://*.mathoverflow.com/*
+// @include       http://*.serverfault.com/*
+// @include       http://*.superuser.com/*
+// @include       http://*.stackapps.com/*
+// @include       http://*.askubuntu.com/*
+// @version       1.0
+// ==/UserScript==
+
+(function () {
+    
+    if(!$.fn.addBack) $.fn.addBack = function(selector){
+        return this.andSelf().filter(selector);    
+    };
+
+    function UpdateTask(target) {
+        this.target = $(target);
+        this.questionId = this.target.data("questionid") || $(".question-page [data-questionid]").data("questionid");
+        this.answerId = this.target.data("answerid");
+    }
+
+    UpdateTask.prototype.addSelf = function () {
+        console.log("going to update post  ", this);
+        var thisTask = this,
+            questionIdPromise = $.Deferred();
+        updateTasks.push(this);
+        updateTasksById[this.answerId || this.questionId] = this;
+        if (this.questionId) {
+            questionIdPromise.resolve(this.questionId);
+        } else {
+            //todo: this is ugly as a sin, even for a fallback. Use [the API][1] instead
+            //[1]: http://api.stackexchange.com/docs/answers-by-ids
+            $.get("/posts/" + this.answerId + "/edit").then(function (response) {
+                questionIdPromise.resolve($(response).find("a.cancel-edit").attr("href"));
+            });
+        }
+        questionIdPromise.then(function (questionId) {
+            if (!updateTasksByQuestionId[questionId]) updateTasksByQuestionId[questionId] = [];
+            updateTasksByQuestionId[questionId].push(thisTask);
+        });
+    };
+
+    UpdateTask.updateAll = function () {
+        var visibilityState = document.visibilityState || document.webkitVisibilityState || "visible";
+        if (visibilityState === "visible") {
+            for (var questionId in updateTasksByQuestionId) {
+                var updateTasksForQuestion = updateTasksByQuestionId[questionId];
+                if ($(updateTasksForQuestion).is(":visible")) {
+                    console.log("going to update posts for "+questionId);
+                    $.get("/questions/" + questionId, {
+                        dataType: "html"
+                    }).then(function (questionId, updateTasksForQuestion, response) {
+                        $response = $(response);
+                        console.log("got a response for "+questionId);
+                        if($(".pager-answers", $response)){
+                            console.log("paging not supported yet");
+                            return;
+                        }
+                        updateTasksForQuestion.forEach(function (updateTask) {
+                            console.log("updating ", updateTask);
+                            var $targetInResponse;
+                            if (updateTask.answerId) {
+                                $targetInResponse = $("#answer-" + updateTask.answerId, $response);
+                            } else {
+                                $targetInResponse = $("#question", $response);
+                            }
+                            //yep, even deleted questions are `.deleted-answer`s.
+                            if ($targetInResponse.is(":not(.deleted-answer)")) {
+                                updateTask.target.removeClass("deleted-answer");
+                            } else {
+                                updateTask.target.addClass("deleted-answer");
+                            }
+                        });
+                    }.bind(null, questionId, updateTasksForQuestion)).fail(function (jqxhr) {
+                        console.log(arguments);
+                    });
+                }
+            }
+        }
+    };
+
+    ///
+
+    var UPDATE_TARGET_SELECTOR = "div[data-answerid], div[data-questionid]",
+
+        updateTasks = [],
+        updateTasksById = {},
+        updateTasksByQuestionId = {};
+
+    ///
+
+    $(UPDATE_TARGET_SELECTOR).each(function () {
+        new UpdateTask(this).addSelf();
+    });
+
+    new MutationObserver(function (records) {
+        records.forEach(function (record) {
+            $(record.addedNodes)
+                .find(UPDATE_TARGET_SELECTOR).addBack(UPDATE_TARGET_SELECTOR)
+                .each(function () {
+                new UpdateTask(this).addSelf();
+            });
+        });
+    }).observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+
+    setInterval(UpdateTask.updateAll, 10000);
+})();
