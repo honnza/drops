b5f0aee559907fc47da77569caa3550460948f89
Jan Dvorak <honnza@gmail.com>
2014-11-07 17:11:53 +0100

    0.3 - improved CSS

--- se-chat-iframe-popup.user.js
+++ se-chat-iframe-popup.user.js
@@ -6,46 +6,54 @@
 // @include       http://chat.meta.stackexchange.com/rooms/*
 // @include       http://chat.stackoverflow.com/rooms/*
 // @include       http://chat.meta.stackoverflow.com/rooms/*
-// @version       0.2
+// @version       0.3
 // ==/UserScript==
 $(function(){
-  var currentFrame = null;
+  var popup = null;
   $("body").click(function(){
-    if(currentFrame) currentFrame.parentNode.removeChild(currentFrame);
-    currentFrame = null;
+    if(popup) popup.parentNode.removeChild(popup);
+    popup = null;
     console.log("frame removed");
   });
   $("#chat").hoverIntent({
     selector: ".content a", 
     out: function(){console.log("out");},
     over: function(event){
+      function maximise(el){
+        el.style.top = "0px";
+        el.style.left = "0px";
+        el.style.right = "0px";
+        el.style.bottom = "0px";
+        el.style.position = "absolute";
+      }
       console.log("in %o", this);
       if(isSeDomain(this.host)){
-        console.log("domain matches");
-        var overlay = document.createElement("div");
-        overlay.style.background = "rgba(0, 0, 0, 0.5)";
-        overlay.style.position = "fixed";
-        overlay.style.top = 0;
-        overlay.style.left = 0;
-        overlay.style.right = 0;
-        overlay.style.bottom = 0;
-        
         var frame = document.createElement("iframe");
-
-        currentFrame = document.createElement("div");
-        currentFrame.style.margin = 5;
-        currentFrame.style.border = "3px solid black";
-        currentFrame.style.borderRadius = 5;
-        currentFrame.style.padding = 10;
-        currentFrame.appendChild(overlay);
-        currentFrame.appendChild(frame);
+        frame.style.height = "100%";
+        frame.style.width = "100%";
+        frame.style.margin = "auto";
+        
+        var border = document.createElement("div");
+        border.style.margin = "10px";
+        border.style.border = "3px solid black";
+        border.style.borderRadius = "5px";
+        border.style.padding = "10px";
+        maximise(border);
+        border.appendChild(frame);
+        
+        popup = document.createElement("div");
+        popup.style.background = "rgba(0,0,0,0.5)";
+        maximise(popup);
+        popup.style.position = "fixed";
+        popup.style.zIndex = 9;
+        popup.appendChild(border);
         
         GM_xmlhttpRequest({
           url: this.href,
           onload: function(response){
-            document.body.appendChild(currentFrame);
-            frame.contentDocument.innerHTML = response.data;
-            console.log("%o appended", currentFrame);
+            document.body.appendChild(popup);
+            frame.contentDocument.innerHTML = response.responseText;
+            console.log("%o appended", popup);
           }
         });
       }
