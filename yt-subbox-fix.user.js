69375611b708256c81340abf8311ee9280dc8b58
Jan Dvorak <honnza@gmail.com>
2015-11-23 01:40:47 +0100

    yt-subbox-fix v11 - awaiting the guide count element, take 1

--- yt-subbox-fix.user.js
+++ yt-subbox-fix.user.js
@@ -2,7 +2,7 @@
 // @name          Youtube subbox number patch
 // @description   replaces the (unreliable and since mid-Nov2015 broken) subscription feed new item counter in the menu
 // @include       http*://*.youtube.com/*
-// @version       0.0.10
+// @version       0.0.11
 // ==/UserScript==
 
 if(location.pathname === "/feed/subscriptions"){
@@ -15,14 +15,10 @@ if(location.pathname === "/feed/subscriptions"){
   xhr.onload = function(){
     var subVids = scrapeIDs(new DOMParser().parseFromString(xhr.response, "text/html"));
     var numUnseen = subVids.filter(id => !~seenVids.indexOf(id)).length;
-    var numElement = document.querySelector("#subscriptions-guide-item .guide-count-value")
-    debugger;
-    if(numElement){
+    awaitElement(document.getElementById("subscriptions-guide-item"), ".guide-count-value", function(){
     	numElement.textContent = numUnseen.toString();
     	console.log("successfully updated the subscriptions guide count")
-    } else {
-    	console.error("want to update the subscriptions guide count but no guide count value is already present");
-    }
+    })
   };
   xhr.onerror = console.error.bind(console);
   xhr.send();
@@ -30,4 +26,27 @@ if(location.pathname === "/feed/subscriptions"){
 
 function scrapeIDs(document){
   return [].map.call(document.querySelectorAll(".section-list .yt-lockup"), elem => elem.dataset.contextItemId)
+}
+
+function awaitElement(parent, selector, callback){
+	var node = parent.querySelector(selector); 
+	if(node){
+		console.log("awaited element exists");
+		callback(node);
+	}else{
+		console.log("awaiting element")
+		mo = new MutationObserver(function(records){
+			records.forEach(function(record){
+				[].some.call(record.addedNodes, function(node){
+					if(node.matches(selector)){
+						callback(node);
+						mo.unobserve();
+						return true;
+					}
+					return false;
+				});
+			});
+		});
+		mo.observe(parent, {childList:true, subtree:true})
+	}
 }
