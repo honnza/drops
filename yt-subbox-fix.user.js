19321d135f040f782a8fe06d8d73bf4c1bdd3a51
Jan Dvorak <honnza@gmail.com>
2015-11-23 06:33:34 +0100

    yt-subbox-fix v17 - apparently adding a complex subtree _doesn't_ launch a giant salvo of node addition observations. Oops.

--- yt-subbox-fix.user.js
+++ yt-subbox-fix.user.js
@@ -2,22 +2,22 @@
 // @name          Youtube subbox number patch
 // @description   replaces the (unreliable and since mid-Nov2015 broken) subscription feed new item counter in the menu
 // @include       http*://*.youtube.com/*
-// @version       0.0.16
+// @version       0.0.17
 // ==/UserScript==
 
 if(location.pathname === "/feed/subscriptions"){
   GM_setValue("seenVids", JSON.stringify(scrapeIDs(document)));
   console.log("uptdated the list of seen vids");
 } else {
-	var seenVids = JSON.parse(GM_getValue("seenVids"));
+   var seenVids = JSON.parse(GM_getValue("seenVids"));
   var xhr = new XMLHttpRequest();
   xhr.open("get", "/feed/subscriptions");
   xhr.onload = function(){
     var subVids = scrapeIDs(new DOMParser().parseFromString(xhr.response, "text/html"));
     var numUnseen = subVids.filter(id => !~seenVids.indexOf(id)).length;
     awaitElement(document.getElementById("guide"), ".guide-count-value", function(numElement){
-    	numElement.textContent = numUnseen.toString();
-    	console.log("successfully updated the subscriptions guide count")
+       numElement.textContent = numUnseen.toString();
+       console.log("successfully updated the subscriptions guide count")
     })
   };
   xhr.onerror = console.error.bind(console);
@@ -29,25 +29,30 @@ function scrapeIDs(document){
 }
 
 function awaitElement(parent, selector, callback){
-	var node = parent.querySelector(selector); 
-	if(node){
-		console.log("awaited element exists");
-		callback(node);
-	}else{
-		console.log("awaiting element")
-		mo = new MutationObserver(function(records){
-			records.forEach(function(record){
-				[].some.call(record.addedNodes, function(node){
-					console.log("new %s element (type %d) #%s observed", node.tagName, node.nodeType, node.id)
-					if(node.nodeType === node.ELEMENT_NODE && node.matches(selector)){
-						callback(node);
-						mo.unobserve();
-						return true;
-					}
-					return false;
-				});
-			});
-		});
-		mo.observe(parent, {childList:true, subtree:true})
-	}
+   var node = parent.querySelector(selector); 
+   if(node){
+      console.log("awaited element exists");
+      callback(node);
+   }else{
+      console.log("awaiting element")
+      mo = new MutationObserver(function(records){
+         records.forEach(function(record){
+            [].some.call(record.addedNodes, function(node){
+               console.log("new %s element (type %d) #%s observed", node.tagName, node.nodeType, node.id)
+               if(node.nodeType === node.ELEMENT_NODE){
+                  if(! node.matches(selector)){
+                     node = node.querySelector(selector);
+                  }
+                  if(node){
+                    callback(node);
+                    mo.unobserve();
+                    return true;
+                  }
+               }
+               return false;
+            });
+         });
+      });
+      mo.observe(parent, {childList:true, subtree:true})
+   }
 }
