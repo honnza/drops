5eed4691a4d6c93aab3762002b78a17fa9e599eb
Jan Dvorak <honnza@gmail.com>
2016-04-13 10:42:41 +0200

    1.5.2 - also fix a specificity issue for messages that are .mine

--- so-chat-user-colors.user.js
+++ so-chat-user-colors.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       SOChatUserColors
-// @version    1.5.1
+// @version    1.5.2
 // @description  color chat lines in a Stack Overflow chat room, using a different color for each user
 // @match      *://chat.stackoverflow.com/rooms/*
 // @match      *://chat.stackexchange.com/rooms/*
@@ -57,7 +57,7 @@ var main = function(){
             +','  +parseInt(('11'+usrIDbits.replace(/.?(.)?.?/g,'$1')+"00000000").slice(0,8),2)
             +','  +parseInt(('11'+usrIDbits.replace(/.?.?(.)?/g,'$1')+"00000000").slice(0,8),2)
             +')';
-            var usrClass = "#chat .user-"+usrId;
+            var usrClass = "#chat .monologue.user-"+usrId;
             newCSS += usrClass + selectorRest + "{background-color:"+usrColor+"}\n";
         }
         CHAT.RoomUsers.allPresent().forEach(function(user){
