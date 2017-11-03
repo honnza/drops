61770fc11eabfdc15860ca8d5a333954dad8b423
Jan Dvorak <honnza@gmail.com>
2017-11-03 17:45:22 +0100

    euler11

--- Euler/common.hs
+++ Euler/common.hs
@@ -1,3 +1,7 @@
 module Common where
 
+fib :: [Integer]
 fib = 1 : 2 : zipWith (+) fib (tail fib) :: [Integer]
+
+parseSSV :: Read a => String -> [[a]]
+parseSSV str = map (map read) $ map words $ lines str
