cc9e43803bd9574b4df67bbc26bf592b73f6acb1
Jan Dvorak <honnza@gmail.com>
2017-11-03 15:50:36 +0100

    euler2

--- Euler/Euler-0-99.hs
+++ Euler/Euler-0-99.hs
@@ -1,3 +1,6 @@
 e01 = print $ sum [x | x <- [0..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 
-main = e01
+e02 = print $ sum [x | x <- takeWhile (< 4000000) fib, even x]
+  where fib = 1 : 2 : zipWith (+) fib (tail fib) :: [Integer]
+
+main = e02
