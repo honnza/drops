21b90c90b19860a60a803ad6efc7733b076d307c
Jan Dvořák <honnza@gmail.com>
2024-09-18 14:45:35 +0200

    add inline rule definition support

--- wavefunction-collapse.rb
+++ wavefunction-collapse.rb
@@ -760,11 +760,17 @@ if $0 == __FILE__
         p $!
         p $@
       end
-    when /^add rule$/
+    when /^add rule(?: (.+))?$/
       begin
-        strs = []
-        loop{str = gets.chomp; break if str.empty?; strs << str}
-        strs = strs.flat_map{|str| str =~ /^(.+)\*\*(\d+)$/ ? [$1] * $2.to_i : [str]}
+        if $1.nil?
+          strs = []
+          loop{str = gets.chomp; break if str.empty?; strs << str}
+        else
+          strs = [$1]
+        end
+        strs = strs
+          .flat_map{|str| str.split %r"(?<!/)//(?!/)"}
+          .flat_map{|str| str =~ /^(.+)\*\*(\d+)$/ ? [$1] * $2.to_i : [str]}
         rule_tiles = strs.map do |row_str|
           row_str.gsub!(/(\S)+\*(\d+)/){([$1] * $2.to_i).join " "}
           row_str.split(" ").map do |tile_str|
@@ -871,7 +877,8 @@ available commands:
 new ruleset [124][/-\\|]? - reset all rules and tiles and set the rotation symmetry order and mirror plane for all rules.
 add tile (name) - create a tile with a given name. If rules have symmetry, asks for the transformed versions of the tile.
 add symmetry (permutation) - define a set of tile substitutions that leave the rules unchanged. Separate tiles within a cycle with /. Separate cycles in a set by spaces.
-add rule - define a pattern that may not appear in the generated pattern. Follow by a list of tile names. Separate multiple tiles an the same position with /. Type / followed by a list to include all tiles except the ones listed. Type only / to include all tiles at that position. Use (tiles)*(number) to duplicate that tile in the pattern. Use (row)**(number) to duplicate an entire row.
+add rule - define a pattern that may not appear in the generated pattern. Follow by a list of tile names. Separate multiple tiles an the same position with /. Type / followed by a list to include all tiles except the ones listed. Type only / to include all tiles at that position. Use (tiles)*(number) to duplicate that tile in the pattern. Use (row)**(number) to duplicate an entire row. Use // to separate multiple rows of rule pattern in one row of input.
+add rule (rule) - as above, but only one line of input.
 
 Ruleset must be defined before tiles, tiles must be defined before tile symmetries that use them, symmetries must be defined before rules.
 
