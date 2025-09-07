545c24f315d69191c86c57a27973d7859396681c
Jan Dvořák <honnza@gmail.com>
2025-09-07 11:23:06 +0200

    add progress bar to image-relax

--- image.relax.rb
+++ image.relax.rb
@@ -1,7 +1,24 @@
 require "io/console"
 require "chunky_png"
 
-def display w, h, buf, gamma
+def progress_bar progress, text = "", width = IO.console.winsize[1] - 1
+  on_cells = ((width - 2) * progress.clamp(0 .. 1))
+  if on_cells > text.length
+    text = text.ljust(width - 2)
+    text[on_cells.floor] = (0x2590 - on_cells % 1 * 8).floor.chr(Encoding::UTF_8) if on_cells % 1 != 0
+    (text + "]").insert(on_cells.floor, "\e[0m")
+                .insert(0, "[\e[107;30m")
+  else
+    bg = (on_cells % 1 * 256).floor
+    fg = bg > 127 ? 30 : 97
+    (text.ljust(width - 2) + "]")
+      .insert(on_cells.floor + 1, "\e[0m")
+      .insert(on_cells.floor, "\e[48;2;#{bg};#{bg};#{bg};#{fg}m")
+      .insert(0, "[\e[107;30m")
+  end
+end
+
+def display_buf w, h, buf, gamma
   igamma = 1 / gamma
   (0 ... (h + 1)/2).each do |hri|
     (0 ... w).each do |ci|
@@ -86,17 +103,22 @@ begin
       end
     end
     max = 255 ** gamma
+    max_diff = 0.0
     buf.each_index do
       unless mask[_1 / 3]
         buf[_1] += diff_buf[_1] / 8
         buf[_1] = buf[_1].clamp(0.0, max)
+        max_diff = diff_buf[_1].abs if max_diff < diff_buf[_1].abs
       end
     end
     IO.console.cursor = [0, 0]
-    display img.width, img.height, buf, gamma if t % 16 == 0
+    if t % 16 == 0
+      display_buf img.width, img.height, buf, gamma
+      puts progress_bar Math::log(max_diff / max, Float::EPSILON), max_diff.to_s
+    end
   end
 rescue Interrupt
 ensure
   puts "\e[?25h\e[?1049l"
-  display img.width, img.height, buf, gamma
+  display_buf img.width, img.height, buf, gamma
 end
